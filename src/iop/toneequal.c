9dd85414fbf3157b7f30665c0cf4a9bc612fd712
Aurélien PIERRE <aurelien@aurelienpierre.com>
2018-12-18 16:04:42 -0500

    remove the blur for now, optimize the code and add more channels

--- src/iop/toneequal.c
+++ src/iop/toneequal.c
@@ -49,7 +49,7 @@ DT_MODULE_INTROSPECTION(1, dt_iop_toneequalizer_params_t)
 
 typedef struct dt_iop_toneequalizer_params_t
 {
-  float deep_blacks, blacks, shadows, midtones, highlights, whites;
+  float ultra_deep_blacks, deep_blacks, blacks, shadows, midtones, highlights, whites;
   float blending;
   int scales;
 } dt_iop_toneequalizer_params_t;
@@ -67,7 +67,7 @@ typedef struct dt_iop_toneequalizer_global_data_t
 
 typedef struct dt_iop_toneequalizer_gui_data_t
 {
-  GtkWidget *deep_blacks, *blacks, *shadows, *midtones, *highlights, *whites;
+  GtkWidget *ultra_deep_blacks, *deep_blacks, *blacks, *shadows, *midtones, *highlights, *whites;
   GtkWidget *blending, *scales;
 } dt_iop_toneequalizer_gui_data_t;
 
@@ -77,17 +77,15 @@ const char *name()
   return _("tone equalizer");
 }
 
-int flags()
-{
-  return IOP_FLAGS_SUPPORTS_BLENDING | IOP_FLAGS_INCLUDE_IN_STYLES | IOP_FLAGS_ALLOW_TILING;
-}
-
 int groups()
 {
   return dt_iop_get_group("tone equalizer", IOP_GROUP_BASIC);
 }
 
-#define GAUSS(a, b, c, x) (a * expf((-(x - b) * (x - b) / (c*c))))
+#ifdef _OPENMP
+#pragma omp declare simd inbranch
+#endif
+#define GAUSS(b, x) (expf((-(x - b) * (x - b) / 4.0f)))
 
 // From data/kernels/extended.cl
 static inline float fastlog2(float x)
@@ -104,90 +102,63 @@ static inline float fastlog2(float x)
     - 1.72587999f / (0.3520887068f + mx.f);
 }
 
+// Build the luma channels : band-pass filters with gaussian windows of std 2 EV, spaced by 2 EV
+const float centers[12] = { -16.0f, -14.0f, -12.0f, -10.0f, -8.0f, -6.0f, \
+                            -4.0f,  - 2.0f,   0.0f,  2.0f,   4.0f,  8.0f};
+
+// For every pixel luminance, the sum of the gaussian masks
+// evenly spaced by 2 EV with 2 EV std should be this constant
+#define w_sum 1.772637204826214f
+
 void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const void *const ivoid,
              void *const ovoid, const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out)
 {
   const dt_iop_toneequalizer_data_t *const d = (const dt_iop_toneequalizer_data_t *const)piece->data;
-  const float deep_blacks = d->params.deep_blacks;
-  const float blacks = d->params.blacks;
-  const float shadows = d->params.shadows;
-  const float midtones = d->params.midtones;
-  const float highlights = d->params.highlights;
-  const float whites = d->params.whites;
 
-  const float std_luma = d->params.blending;
+  const float factors[12] = { 1.0f,
+                              1.0f,
+                              exp2f(d->params.ultra_deep_blacks),
+                              exp2f(d->params.deep_blacks),
+                              exp2f(d->params.blacks),
+                              exp2f(d->params.shadows),
+                              exp2f(d->params.midtones),
+                              exp2f(d->params.highlights),
+                              exp2f(d->params.whites),
+                              1.0f,
+                              1.0f,
+                              1.0f };
 
   const int ch = piece->colors;
-  const int scales = d->params.scales;
-  const int width = roi_in->width, height = roi_in->height;
+  const float *const in = (const float *const)ivoid;
+  float *const out = (float *const)ovoid;
 
-  memcpy(ovoid, ivoid, (size_t)ch * sizeof(float) * width * height);
-
-  for(int s = 0; s < scales; ++s)
+  // correct the luminance
+#ifdef _OPENMP
+#pragma omp parallel for SIMD() default(none) schedule(simd:static)
+#endif
+  for(size_t k = 0; k < (size_t)ch * roi_out->width * roi_out->height; k += ch)
   {
-    const int window = scales - s;
-    const float std_spatial = 2.0f * window + 1.0f;
-    const float norm = 1.0f / (std_spatial * powf(2.0f * M_PI, 0.5f));
+    // get the luminance of the pixel
+    const float luma = fastlog2( (fmaxf(fmaxf(in[k], in[k+1]), in[k+2]) +
+                                  fminf(fminf(in[k], in[k+1]), in[k+2])) / 2.0f );
+
+    // build the pixel correction
+    float correction = 0.0f;
 
 #ifdef _OPENMP
-#pragma omp parallel for SIMD() default(none) schedule(static)
+#pragma omp ordered simd
 #endif
-    for(int j = 0; j < height; j++)
-    {
-      for(int i = 0; i < width; i++)
-      {
-        const float *in = ((float *)ivoid) + (size_t)ch * (width * j + i);
-        float *out = ((float *)ovoid) + (size_t)ch * (width * j + i);
+    for (int c = 0; c < 12; ++c) correction += GAUSS(centers[c], luma) * factors[c];
 
-        // get the luminance of the pixel
-        const float luma = fastlog2((fmaxf(fmaxf(out[0], out[1]), out[2]) + fminf(fminf(out[0], out[1]), out[2])) / 2.0f);
+    correction /= w_sum;
 
-        // build the channels masks
-        const float w_deep_blacks = GAUSS(1.0f, -10.0f, std_luma, luma);
-        const float w_black = GAUSS(1.0f, -8.0f, std_luma, luma);
-        const float w_shadows = GAUSS(1.0f, -6.0f, std_luma, luma);
-        const float w_midtones = GAUSS(1.0f, -4.0f, std_luma, luma);
-        const float w_highlights = GAUSS(1.0f, -2.0f, std_luma, luma);
-        const float w_whites = GAUSS(1.0f, 0.0f, std_luma, luma);
-        const float w_sum = (w_black + w_shadows + w_midtones + w_highlights + w_whites);
-        const float correction = (deep_blacks * w_deep_blacks + blacks * w_black + shadows * w_shadows + midtones * w_midtones + highlights * w_highlights + whites * w_whites);
-
-        for(int c = 0; c < 3; c++)
-        {
-          // Build the gaussian pyramid
-          float collector = 0.0f;
-
-          if(j > window && (height - j) > window && i > window && (width - i) > window)
-          {
-            // Convolve a gaussian blur
-            for(int m = -window; m < window + 1; ++m)
-            {
-              for(int n = -window; n < window + 1; ++n)
-              {
-                const float M = GAUSS(norm, 0.0f, std_spatial, m);
-                const float N = GAUSS(norm, 0.0f, std_spatial, n);
-                collector += M * N * (((float *)ivoid) + (size_t)ch * (width * (j + m) + (i + n)))[c];
-              }
-            }
-            collector /= std_spatial * std_spatial;
-          }
-          else
-          {
-            collector = out[c];
-          }
-
-          // Apply the weighted contribution of each channel to the current pixel
-          const float high_freq = (in[c] - collector);
-          const float low_freq_luma = correction * collector / w_sum;
-          const float all_freq_luma = correction * out[c] / w_sum;
-          out[c] = (high_freq + low_freq_luma + all_freq_luma) / 2.0f;
-        }
-      }
-    }
+    // Apply the weighted contribution of each channel to the current pixel
+#ifdef _OPENMP
+#pragma omp ordered simd
+#endif
+    for (int c = 0; c < 3; ++c) out[k + c] = correction * in[k + c];
+    out[k + 4] = in[k + 4];
   }
-
-  if(piece->pipe->mask_display & DT_DEV_PIXELPIPE_DISPLAY_MASK)
-    dt_iop_alpha_copy(ivoid, ovoid, roi_out->width, roi_out->height);
 }
 
 void init_global(dt_iop_module_so_t *module)
@@ -230,14 +201,13 @@ void gui_update(struct dt_iop_module_t *self)
   dt_iop_toneequalizer_gui_data_t *g = (dt_iop_toneequalizer_gui_data_t *)self->gui_data;
   dt_iop_toneequalizer_params_t *p = (dt_iop_toneequalizer_params_t *)module->params;
 
+  dt_bauhaus_slider_set(g->ultra_deep_blacks, p->ultra_deep_blacks);
   dt_bauhaus_slider_set(g->deep_blacks, p->deep_blacks);
   dt_bauhaus_slider_set(g->blacks, p->blacks);
   dt_bauhaus_slider_set(g->shadows, p->shadows);
   dt_bauhaus_slider_set(g->midtones, p->midtones);
   dt_bauhaus_slider_set(g->highlights, p->highlights);
   dt_bauhaus_slider_set(g->whites, p->whites);
-  dt_bauhaus_slider_set(g->blending, p->blending);
-  dt_bauhaus_slider_set(g->scales, p->scales);
 }
 
 void init(dt_iop_module_t *module)
@@ -248,7 +218,7 @@ void init(dt_iop_module_t *module)
   module->priority = 158; // module order created by iop_dependencies.py, do not edit!
   module->params_size = sizeof(dt_iop_toneequalizer_params_t);
   module->gui_data = NULL;
-  dt_iop_toneequalizer_params_t tmp = (dt_iop_toneequalizer_params_t){1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0f, 6};
+  dt_iop_toneequalizer_params_t tmp = (dt_iop_toneequalizer_params_t){0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   memcpy(module->params, &tmp, sizeof(dt_iop_toneequalizer_params_t));
   memcpy(module->default_params, &tmp, sizeof(dt_iop_toneequalizer_params_t));
 }
@@ -257,6 +227,15 @@ void cleanup(dt_iop_module_t *module)
 {
   free(module->params);
   module->params = NULL;
+}
+
+static void ultra_deep_blacks_callback(GtkWidget *slider, gpointer user_data)
+{
+  dt_iop_module_t *self = (dt_iop_module_t *)user_data;
+  if(self->dt->gui->reset) return;
+  dt_iop_toneequalizer_params_t *p = (dt_iop_toneequalizer_params_t *)self->params;
+  p->ultra_deep_blacks = dt_bauhaus_slider_get(slider);
+  dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
 
 static void deep_blacks_callback(GtkWidget *slider, gpointer user_data)
@@ -313,24 +292,6 @@ static void whites_callback(GtkWidget *slider, gpointer user_data)
   dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
 
-static void blending_callback(GtkWidget *slider, gpointer user_data)
-{
-  dt_iop_module_t *self = (dt_iop_module_t *)user_data;
-  if(self->dt->gui->reset) return;
-  dt_iop_toneequalizer_params_t *p = (dt_iop_toneequalizer_params_t *)self->params;
-  p->blending = dt_bauhaus_slider_get(slider);
-  dt_dev_add_history_item(darktable.develop, self, TRUE);
-}
-
-static void scales_callback(GtkWidget *slider, gpointer user_data)
-{
-  dt_iop_module_t *self = (dt_iop_module_t *)user_data;
-  if(self->dt->gui->reset) return;
-  dt_iop_toneequalizer_params_t *p = (dt_iop_toneequalizer_params_t *)self->params;
-  p->scales = dt_bauhaus_slider_get(slider);
-  dt_dev_add_history_item(darktable.develop, self, TRUE);
-}
-
 void gui_init(struct dt_iop_module_t *self)
 {
   self->gui_data = malloc(sizeof(dt_iop_toneequalizer_gui_data_t));
@@ -338,50 +299,47 @@ void gui_init(struct dt_iop_module_t *self)
 
   self->widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, DT_GUI_IOP_MODULE_CONTROL_SPACING);
 
-  g->deep_blacks = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->deep_blacks, NULL, _("deep blacks"));
+  g->ultra_deep_blacks = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->ultra_deep_blacks, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->ultra_deep_blacks, NULL, _("-12 EV : HDR ultra-deep blacks"));
+  gtk_box_pack_start(GTK_BOX(self->widget), g->ultra_deep_blacks, TRUE, TRUE, 0);
+  g_signal_connect(G_OBJECT(g->ultra_deep_blacks), "value-changed", G_CALLBACK(ultra_deep_blacks_callback), self);
+
+  g->deep_blacks = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->deep_blacks, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->deep_blacks, NULL, _("-10 EV : HDR deep blacks"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->deep_blacks, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->deep_blacks), "value-changed", G_CALLBACK(deep_blacks_callback), self);
 
-  g->blacks = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->blacks, NULL, _("blacks"));
+  g->blacks = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->blacks, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->blacks, NULL, _("-08 EV : blacks"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->blacks, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->blacks), "value-changed", G_CALLBACK(blacks_callback), self);
 
-  g->shadows = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->shadows, NULL, _("shadows"));
+  g->shadows = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->shadows, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->shadows, NULL, _("-06 EV : shadows"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->shadows, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->shadows), "value-changed", G_CALLBACK(shadows_callback), self);
 
-  g->midtones = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->midtones, NULL, _("midtones"));
+  g->midtones = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->midtones, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->midtones, NULL, _("-04 EV : midtones"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->midtones, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->midtones), "value-changed", G_CALLBACK(midtones_callback), self);
 
-  g->highlights = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->highlights, NULL, _("highlights"));
+  g->highlights = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->highlights, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->highlights, NULL, _("-02 EV : highlights"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->highlights, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->highlights), "value-changed", G_CALLBACK(highlights_callback), self);
 
-  g->whites = dt_bauhaus_slider_new_with_range(self, 0.0, 2.0, 0.1, 1.0, 2);
-  dt_bauhaus_widget_set_label(g->whites, NULL, _("whites"));
+  g->whites = dt_bauhaus_slider_new_with_range(self, -2.0, 2.0, 0.1, 0.0, 2);
+  dt_bauhaus_slider_set_format(g->whites, "%+.2f EV");
+  dt_bauhaus_widget_set_label(g->whites, NULL, _("-00 EV : whites"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->whites, TRUE, TRUE, 0);
   g_signal_connect(G_OBJECT(g->whites), "value-changed", G_CALLBACK(whites_callback), self);
-
-  gtk_box_pack_start(GTK_BOX(self->widget), dt_ui_section_label_new(_("advanced settings")), FALSE, FALSE, 5);
-
-  g->blending = dt_bauhaus_slider_new_with_range(self, 1.0, 3.0, 1., 2.0, 1);
-  dt_bauhaus_widget_set_label(g->blending, NULL, _("luminance blending"));
-  dt_bauhaus_slider_set_format(g->blending, "%.1f EV");
-  gtk_box_pack_start(GTK_BOX(self->widget), g->blending, TRUE, TRUE, 0);
-  g_signal_connect(G_OBJECT(g->blending), "value-changed", G_CALLBACK(blending_callback), self);
-
-  g->scales = dt_bauhaus_slider_new_with_range(self, 1, 11, 1, 6, 0);
-  dt_bauhaus_widget_set_label(g->scales, NULL, _("spatial blending"));
-  dt_bauhaus_slider_set_format(g->scales, "%.0f px");
-  gtk_widget_set_tooltip_text(g->scales, _("this affects the performance"));
-  gtk_box_pack_start(GTK_BOX(self->widget), g->scales, TRUE, TRUE, 0);
-  g_signal_connect(G_OBJECT(g->scales), "value-changed", G_CALLBACK(scales_callback), self);
 }
 
 void gui_cleanup(struct dt_iop_module_t *self)
